7cb03e93ff5035bc84701b5d8079f6d03e226d70
mckay <simi7236@gmail.com>
2020-06-26 19:28:13 +0900

    selection, quick sort

---
+++ Quick Sort.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+def func(array, left, right)
+  pivot = left
+  low = left + 1
+  high = right
+
+  if right - left > 0
+    while low < high
+      low += 1 while array[low] <= array[pivot] && low < right
+      high -= 1 while array[high] > array[pivot] && high > left
+      if low < high
+        temp = array[low]
+        array[low] = array[high]
+        array[high] = temp
+      end
+    end
+    if array[high] <= array[pivot]
+      temp = array[pivot]
+      array[pivot] = array[high]
+      array[high] = temp
+      pivot = high
+    end
+    func(array, left, pivot - 1)
+    func(array, pivot + 1, right)
+  end
+end
+
+# rand_arr = [1, 0, 10, 2, 10, 7, 1, 6, 2, 8]
+
+rand_arr = []
+10.times do
+  rand_arr.push(Random.new.rand(0..10))
+end
+
+p rand_arr
+func(rand_arr, 0, rand_arr.length - 1)
+
+print rand_arr
---
+++ Selection Sort.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+# array1 = [5, 7, 2, 9, 1, 4, 0, 3, 8, 6]
+
+array1 = []
+10.times do
+  array1.push(Random.new.rand(0..10))
+end
+p array1
+
+(0..array1.length - 2).each do |i|
+  (i + 1..array1.length - 1).each do |j|
+    next unless array1[i] > array1[j]
+
+    temp = array1[i]
+    array1[i] = array1[j]
+    array1[j] = temp
+  end
+end
+
+print array1
