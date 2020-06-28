45ecd30c1c9514808405c5a564274e226626a4be
mckay <simi7236@gmail.com>
2020-06-28 22:19:51 +0900

    Insertion Sort

--- Insertion Sort.rb
+++ Insertion Sort.rb
@@ -5,3 +5,28 @@ arr = []
   arr.push(Random.new.rand(0..10))
 end
 p arr
+
+# 1
+# (1..arr.length - 1).each do |i|
+#   (i - 1).downto(0) do |j|
+#     next unless arr[i] < arr[j]
+#
+#     temp = arr[i]
+#     arr[i] = arr[j]
+#     arr[j] = temp
+#     i = j
+#   end
+# end
+
+# 2
+(1..arr.length - 1).each do |i|
+  val1 = arr[i]
+  (i - 1).downto(0) do |j|
+    if val1 < arr[j]
+      arr[j + 1] = arr[j]
+      arr[j] = val1
+    end
+  end
+end
+
+p arr
