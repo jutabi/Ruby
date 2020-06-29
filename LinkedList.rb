d6add2eb9a05144683b472975c9f82d65dab59cd
mckay <simi7236@gmail.com>
2020-06-29 20:29:21 +0900

    Linked List

---
+++ LinkedList.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+class Node
+  attr_accessor :prev, :value, :next
+  def initialize(left, value, right)
+    @prev = left
+    @value = value
+    @next = right
+  end
+end
+
+class LinkedList
+  def initialize(value)
+    @head = Node.new(nil, value, nil)
+  end
+
+  def insert_node(value)
+    cur_node = @head
+    cur_node = cur_node.next until cur_node.next.nil?
+    cur_node.next = Node.new(cur_node, value, nil)
+  end
+
+  def delete_node(value)
+    cur_node = @head
+    if cur_node.value == value
+      if cur_node.next.nil?
+        @head = nil
+      else
+        @head = cur_node.next
+        @head.prev = nil
+      end
+    else
+      until cur_node.next.nil?
+        cur_node = cur_node.next
+        next unless cur_node.value == value
+
+        cur_node.prev.next = cur_node.next
+        cur_node.next.prev = cur_node.prev unless cur_node.next.nil?
+      end
+    end
+  end
+
+  def read_nodes
+    cur_node = @head
+    until cur_node.nil?
+      print "#{cur_node.value},"
+      cur_node = cur_node.next
+    end
+    puts
+  end
+end
+
+list = LinkedList.new(0)
+list.insert_node(1)
+list.insert_node(2)
+list.insert_node(1)
+list.insert_node(3)
+print 'result: '
+list.read_nodes
+
+list.delete_node(1)
+print 'result: '
+list.read_nodes
+
+list.delete_node(0)
+print 'result: '
+list.read_nodes
+
+list.delete_node(3)
+print 'result: '
+list.read_nodes
+
+list.delete_node(2)
+print 'last result: '
+list.read_nodes
